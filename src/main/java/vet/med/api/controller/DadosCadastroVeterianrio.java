54d303971ee47308288bfd24e6b901db48d8e07d
Vanessazanco <vanessazancof@gmail.com>
2024-09-18 12:10:41 +0100

    Vet Record

---
+++ src/main/java/vet/med/api/controller/DadosCadastroVeterianrio.java
@@ -0,0 +1,4 @@
+package vet.med.api.controller;
+
+public record DadosCadastroVeterianrio() {
+}
--- src/main/java/vet/med/api/controller/VeterinarioController.java
+++ src/main/java/vet/med/api/controller/VeterinarioController.java
@@ -1,4 +1,18 @@
 package vet.med.api.controller;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("veterinarios")
 public class VeterinarioController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroVeterianrio dados){
+
+    }
+
 }
