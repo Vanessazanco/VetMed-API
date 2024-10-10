b5f57acb97d8d02a781f7dcc033cfbce32cd83db
Vanessazanco <vanessazancof@gmail.com>
2024-10-10 13:10:25 +0100

    Vet

--- src/main/java/vet/med/api/endereco/Endereco.java
+++ src/main/java/vet/med/api/endereco/Endereco.java
@@ -1,4 +1,16 @@
 package vet.med.api.endereco;
 
+import jakarta.persistence.Embeddable;
+
+
+@Embeddable
 public class Endereco {
+    private String logadouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
 }
--- src/main/java/vet/med/api/medico/Veterinario.java
+++ src/main/java/vet/med/api/medico/Veterinario.java
@@ -1,12 +1,27 @@
 package vet.med.api.medico;
 
+import jakarta.persistence.*;
+import lombok.*;
 import vet.med.api.endereco.Endereco;
 
+
+@Table(name = "veterinarios")
+@Entity(name = "Veterinario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Veterinario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crmv;
+
+    @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
+
+    @Embedded
     private Endereco endereco;
 }
